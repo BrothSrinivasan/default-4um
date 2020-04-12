bb216f2678d1b07583ef28ef24038d840ed7685b
BrothSrinivasan <barath5999@gmail.com>
2020-04-11 22:43:42 -0400

    final changes

--- index.js
+++ index.js
@@ -164,6 +164,6 @@ app.get("*", function(req, res) {
     res.render("error", {"message": "404 ERROR: PAGE NOT FOUND"});
 })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port 3000!');
 });
